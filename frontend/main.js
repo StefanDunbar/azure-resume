7387788907dc5d46c60490fff643f7e42f5ba191
Stefan Dunbar <stefanddunbar@googlemail.com>
2023-04-07 17:46:42 +0100

    Updates to main.js

--- frontend/main.js
+++ frontend/main.js
@@ -3,15 +3,14 @@ window.addEventListener('DOMContentLoaded', (event) =>{
 })
 
 const functionApiUrl = 'https://getresumecounter-sd.azurewebsites.net/api/GetResumeCounter?code=nBGZOlBewQuWfay1gZCxfA8_RVdlcZLIS4PR_e1bz5rgAzFudlyo5Q==';
-const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
-    let count = 30;
+    let count = 0;
     fetch(functionApiUrl).then(response => {
-        return response.json;
+        return response.json();
     }).then(response => {
         console.log("Website called function API.")
-        const count = res;
+        count = response.count;
         document.getElementById("counter").innerText = count;
     }).catch(function(error){
         console.log(error);
